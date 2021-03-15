b6bec2580a5b4fe01c2bb0a4ccca50a6107998ba
Daniel Ash <d_ash12345@hotmail.co.uk>
2021-03-15 18:35:16 +0000

    Added last few methods and added driver code.

--- lib/bst.rb
+++ lib/bst.rb
@@ -20,12 +20,15 @@ end
 
 # creates tree structure for BST
 class Tree
+  attr_accessor :array, :root
+
   def initialize(array)
     @array = array.sort.uniq
     @root = build_tree(@array)
   end
 
   def build_tree(array)
+    # builds a BST from an array
     return nil if array.empty?
 
     middle = array.length / 2
@@ -33,6 +36,7 @@ class Tree
   end
 
   def insert(value, node = @root)
+    # inserts a new value in to the tree
     if node.data >= value
       return node.left = Node.new(value, nil, nil) if node.left.nil?
 
@@ -84,69 +88,118 @@ class Tree
   end
 
   def level_order(node = @root, queue = [], array = [])
-    return nil if node.nil?
+    # prints each node in breadth-first level order
+    return node if node.nil?
 
     array.push(node.data)
-    queue.push(node.left)
-    queue.push(node.right)
+    queue.push(node.left) unless node.left == nil
+    queue.push(node.right) unless node.right == nil
     level_order(queue.shift, queue, array)
     array
   end
 
   def preorder(node = @root, array = [])
+    # prints each node in depth-first preorder
     return nil if node.nil?
 
     array.push(node.data)
-    node.left = preorder(node.left, array)
-    node.right = preorder(node.right, array)
+    preorder(node.left, array)
+    preorder(node.right, array)
     array
   end
 
   def inorder(node = @root, array = [])
+     # prints each node in depth-first inorder
     return nil if node.nil?
 
-    node.left = preorder(node.left, array)
+    inorder(node.left, array)
     array.push(node.data)
-    node.right = preorder(node.right, array)
+    inorder(node.right, array)
     array
   end
 
   def postorder(node = @root, array = [])
+     # prints each node in depth-first postorder
     return nil if node.nil?
 
-    node.left = preorder(node.left, array)
-    node.right = preorder(node.right, array)
+    postorder(node.left, array)
+    postorder(node.right, array)
     array.push(node.data)
     array
   end
 
   def depth(value = nil, node = @root, counter = 0)
+     # returns the depth of the specified node in the tree
     return nil if node.nil?
 
     if value < node.data
       counter += 1
-      height(value, node.left, counter)
+      depth(value, node.left, counter)
     elsif value > node.data
       counter += 1
-      height(value, node.right, counter)
+      depth(value, node.right, counter)
     else
       counter
     end
   end
 
+  def height(node = @root)
+    # returns the height of the specified node in the tree
+    return -1 if node.nil?
+
+    [height(node.left), height(node.right)].max + 1
+  end
+
+  def balanced?(node = @root)
+    # checks if the tree is balanced
+    (height(node.left) - height(node.right)).abs > 1 ? false : true
+  end
+
+  def rebalance(node = @root)
+    # rebalances the tree
+    array = level_order(node)
+    array = array.sort
+    Tree.new(array)
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
+    # prints the tree in the terminal
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
 
-# array = []
-# 100.times do
-#   array.push(rand(100))
-# end
-
-array = [1, 2, 9, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+# driver code
+tree = Tree.new(array = Array.new(15) { rand(1..100 )})
 tree = Tree.new(array)
+
 tree.pretty_print
-p tree.height(13)
+
+puts "Balanced: #{tree.balanced?}"
+puts "Level order: #{tree.level_order}"
+puts "Preorder: #{tree.preorder}"
+puts "Inorder: #{tree.inorder}"
+puts "Postorder: #{tree.postorder}"
+
+tree.insert(100)
+tree.insert(1000)
+tree.insert(10000)
+tree.insert(100000)
+tree.pretty_print
+
+puts "Balanced: #{tree.balanced?}"
+
+tree = tree.rebalance
+
+tree.pretty_print
+
+puts "Balanced: #{tree.balanced?}"
+
+puts "Level order: #{tree.level_order}"
+puts "Preorder: #{tree.preorder}"
+puts "Inorder: #{tree.inorder}"
+puts "Postorder: #{tree.postorder}"
+
+
+
